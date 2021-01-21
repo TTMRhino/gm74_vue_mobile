829be71ccf8d613e41b68407a5c7c37bfda713ad
Morozov Dmitriy Viktorovich <DMorozov@gksm.local>
2021-01-21 15:20:58 +0500

    21/01/2021

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,77 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+    
+      <el-col :span="24">     
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+           unique-opened='true'
+           
+          >
+
+
+          <el-submenu index="1">
+
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>Товар</span>
+            </template>
+
+            <el-submenu index="1-1">
+             <template slot="title">Группа№1</template>
+              <el-menu-item index="1-1-1"                
+                @click="clickMenu($event)"
+              >Под_Группа№1
+              </el-menu-item>              
+            </el-submenu>
+
+            <el-submenu index="1-2">
+             <template slot="title">Группа№2</template>
+              <el-menu-item index="1-2-1" 
+              @click="clickMenu($event)"
+              >Под_Группа№1</el-menu-item>
+              <el-menu-item index="1-2-2"
+              @click="clickMenu($event)"
+              >Под_Группа№2</el-menu-item>              
+            </el-submenu>
+
+            <el-submenu index="1-3">
+             <template slot="title">Группа№3</template>
+              <el-menu-item index="1-3-1"
+              @click="clickMenu($event)"
+              >Под_Группа№1</el-menu-item>
+              <el-menu-item index="1-3-2"
+              @click="clickMenu($event)"
+              >Под_Группа№2</el-menu-item>
+              <el-menu-item index="1-3-3"
+              @click="clickMenu($event)"
+              >Под_Группа№3</el-menu-item>               
+            </el-submenu>
+
+          </el-submenu>
+        </el-menu>
+      </el-col>
+
+
     </div>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+  export default {
+  
+    methods: {
+     
+      clickMenu(subgroup){
+        //console.log(subgroup.$el.innerText + " "+ index);
+        console.log(subgroup.$el);       
+       
+      }
+    }
+  }
+</script>
